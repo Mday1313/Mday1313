982cb429c195aa3b82adf6b82aa4dd152dd1f7dd
Mday1313 <melissaday076@gmail.com>
2019-06-26 18:31:24 -0400

    php?

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Demo contact form melissaday076@gmail.com';
 
 // an email address that will receive the email with the output of the form
 $sendTo = 'melissaday076@gmail.com';
